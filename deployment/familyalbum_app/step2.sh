3be9248290f17c984693b2edeaa2da4403ac13b0
Oleg Fayans <ofajans@gmail.com>
2016-03-13 13:53:25 +0100

    Made a usable frontend image

--- deployment/familyalbum_app/step2.sh
+++ deployment/familyalbum_app/step2.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+export PGPASSWORD="Y0unevergueS$"
+export APP_SETTINGS=config.DevelopmentConfig
+export DATABASE_URL="postgresql://JAmesB0nd:$PGPASSWORD@localhost/familyalbum_dev"
+export MEDIA_FOLDER=/data
+export MEDIA_THUMBNAIL_FOLDER=/data/thumbnails
+export FAMILYALBUM_SECRET_KEY='U neveR gues$'
 /usr/sbin/sshd -D 
