a4019402c192e05eba0ac45f7c13724381e190eb
Oleg Fayans <ofajans@gmail.com>
2016-03-18 23:14:10 +0100

    More deployment-related stuff

--- deployment/familyalbum_app/step2.sh
+++ deployment/familyalbum_app/step2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PGPASSWORD="Y0unevergueS$"
 export APP_SETTINGS=config.DevelopmentConfig
-export DATABASE_URL="postgresql://JAmesB0nd:$PGPASSWORD@familyalbum_db/familyalbum_dev"
+export DATABASE_URL="postgresql://jamesb0nd:$PGPASSWORD@familyalbum_db/familyalbum_dev?client_encoding=utf8"
 export MEDIA_FOLDER=/data
 export MEDIA_THUMBNAIL_FOLDER=/data/thumbnails
 export FAMILYALBUM_SECRET_KEY='U neveR gues$'
+/usr/sbin/nginx
 /usr/sbin/sshd -D 
