0531e238d587958f1365eab089c964ef254e4bd6
Oleg Fayans <ofayans@redhat.com>
2016-03-23 09:01:44 +0100

    added deployment script, tweaked nginx to be able to handle big uploadss

---
+++ deployment/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DB_CONTAINER_NAME=familyalbum_db
+DB_IMAGE_NAME=familyalbum_db
+FRONTEND_CONT_NAME=familyalbum_app
+FRONTEND_IMAGE_NAME=familyalbum_app
+LOCAL_DATA=$HOME/data
+
+
+# launch database container
+DB_CONT_NAME=`docker ps -a | grep $DB_IMAGE_NAME | awk '{print $NF}'| tail -n 1`
+if [[ -z "$DB_CONT_NAME" ]]
+# The container has not yet been initialized
+then
+  docker run --name=$DB_CONTAINER_NAME $DB_IMAGE_NAME &
+  sleep 90
+else
+  docker start $DB_CONT_NAME
+fi
+
+# launch frontend container
+APP_CONT_NAME=`docker ps -a | grep $FRONTEND_IMAGE_NAME | awk '{print $NF}'| tail -n 1`
+if [[ -z "$APP_CONT_NAME" ]]
+# The container has not yet been initialized
+then
+docker run -p 80:80 -p 2222:22 --name=$FRONTEND_CONT_NAME --link=$DB_CONTAINER_NAME:$DB_CONTAINER_NAME \
+     -v $LOCAL_DATA:/data:Z $FRONTEND_IMAGE_NAME &
+else
+  docker start $FRONTEND_CONT_NAME
+fi
+
